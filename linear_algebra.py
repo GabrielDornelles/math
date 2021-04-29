2b4f7191a489ad540eb7725bb0bba9894fd21e57
Gabriel <gabriel.monteiro@meta.com.br>
2021-04-29 00:59:39 -0300

    add plan equations and plot

--- linear_algebra.py
+++ linear_algebra.py
@@ -68,10 +68,51 @@ class LinearAlgebraOperations:
         ax.set_zlabel("z")
         plt.show()
 
+    def plan_equation(self, points:list,vp:tuple=None):
+        """
+        Plan equation is defined by: ax+by+cz+d=0
+        input: list of tuples with the given points in 3d space
+        """
+        A = points[0]
+        B = points[1]
+        C = points[2]
+        #create the plan
+        AB= np.subtract(B,A)
+        AC= np.subtract(C,A)
+        normal_vector = self.cross_product(AB,AC)
+        D = -(normal_vector[0]*A[0]+ normal_vector[1]*A[1]+ normal_vector[2]*A[2])
+        print(D)
+        print(normal_vector)
+        if vp is not None:
+            x,y,z=vp
+            contains = (normal_vector[0]*x)+(normal_vector[1]*y)+(normal_vector[2]*z)+D==0
+            answer = "is" if contains else "is not"
+            print(f"the point {vp} {answer} contained by the plan")
+        auto_size = max(abs(normal_vector[0]),abs(normal_vector[1]),abs(normal_vector[2]))
+        x = np.linspace(-auto_size,auto_size,10)
+        y = np.linspace(-auto_size,auto_size,10)
+        X,Y = np.meshgrid(x,y)
+        a,b,c,d = normal_vector[0],normal_vector[1],normal_vector[2],D
+        Z = (d - a*X - b*Y) / c
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        surf = ax.plot_surface(X, Y, Z,color="black")
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("z")
+        O = np.array([0,0,0])
+        ax1 = fig.gca(projection="3d")
+        ax1.quiver(O,O,O,normal_vector[0],normal_vector[1],normal_vector[2],color="red")
+        plt.title("plan and normal vector")
+        plt.show()
+
+vectors = LinearAlgebraOperations()
+vectors.plan_equation(points=[(2,0,1),(3,2,3),(0,4,4)])
+#vectors.plan_equation(points=[(20,12,8),(20,6,10),(0,12,8)])
 vector_1 = np.array([3,0,0])
 vector_2 = np.array([0,2,0])
 vector_3 = np.array([3,2,-4])
-vectors = LinearAlgebraOperations()
+
 
 mixed_product = vectors.mixed_product(vector_1,vector_2,vector_3)
 print(f"mixed product between vectors {vector_1},{vector_2} and {vector_3}: {mixed_product}")
@@ -89,4 +130,5 @@ print(contains)
 
 
 ### plotting to see if it works
-vectors.plot_3_vectors(vector_1,vector_2,vectors.cross_product(vector_1,vector_2))
+#vectors.plot_3_vectors(vector_1,vector_2,vectors.cross_product(vector_1,vector_2))
+
